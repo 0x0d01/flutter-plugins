a2c5989957f0d8ca4d147b75a1afe5ad7e7e9a89
0x0d01 <32776693+0x0d01@users.noreply.github.com>
2023-02-20 09:27:11 +0700

    delegate audio session handling to caller

--- packages/audio_streamer/ios/Classes/SwiftAudioStreamerPlugin.swift
+++ packages/audio_streamer/ios/Classes/SwiftAudioStreamerPlugin.swift
@@ -26,6 +26,21 @@ public class SwiftAudioStreamerPlugin: NSObject, FlutterPlugin, FlutterStreamHan
                   selector: #selector(handleInterruption(notification:)),
                   name: AVAudioSession.interruptionNotification,
                   object: nil)
+      
+    NotificationCenter.default.addObserver( forName:AVAudioSession.routeChangeNotification, object: AVAudioSession.sharedInstance(), queue: nil) { notification in
+      guard let userInfo = notification.userInfo,
+            let reasonValue = userInfo[AVAudioSessionRouteChangeReasonKey] as? UInt,
+            let reason = AVAudioSession.RouteChangeReason(rawValue:reasonValue) else {
+          return
+      }
+      switch reason {
+      case .newDeviceAvailable, .oldDeviceUnavailable:
+          self.engine.stop()
+          self.startRecording()
+          break
+      default: ()
+      }
+    }
   }
 
   @objc func handleInterruption(notification: Notification) {
@@ -65,9 +80,6 @@ public class SwiftAudioStreamerPlugin: NSObject, FlutterPlugin, FlutterStreamHan
 
     func startRecording() {
         engine = AVAudioEngine()
-      
-        try! AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playAndRecord, options: .mixWithOthers)
-        try! AVAudioSession.sharedInstance().setActive(true)
       
         let input = engine.inputNode
         let bus = 0
